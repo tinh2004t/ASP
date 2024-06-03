c55c3cff5f1f0d6a34f15dfeb110a0938be22f46
tinh2004t <117645071+tinh2004t@users.noreply.github.com>
2024-06-03 11:12:19 +0700

    baigiuaky

---
+++ LttBaiDanhGiaGiuaKy/LttBaiDanhGiaGiuaKy/Controllers/LttProductController.cs
@@ -0,0 +1,117 @@
+﻿using LttBaiDanhGiaGiuaKy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LttBaiDanhGiaGiuaKy.Controllers
+{
+    public class LttProductController : Controller
+    {
+        private static List<LttProduct> LttProducts = new List<LttProduct>()
+        {
+            new LttProduct{LttId=204, LttName="Banh Bao", LttImage="../Images/Anhbanhbao.png", LttQuantity=24,LttPrice=10000,LttIsActive=true},
+            new LttProduct{LttId=202, LttName="Banh Ca", LttImage="../Images/Anhbanhca.png", LttQuantity=14,LttPrice=12000,LttIsActive=true}
+        };
+
+        // GET: LttProduct
+        public ActionResult Index()
+        {
+            return View(LttProducts);
+        }
+
+        //Get: Create Products
+        public ActionResult LttCreate()
+        {
+            var LttProduct = new LttProduct();
+            return View(LttProduct);
+        }
+
+        //Post: Create Products
+        [HttpPost]
+        public ActionResult LttCreate(LttProduct lttProduct)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(lttProduct);
+            }
+
+            LttProducts.Add(lttProduct);
+            return RedirectToAction("Index");
+        }
+        //Get: Edit Products
+        [HttpGet]
+        public ActionResult LttEdit(int id)
+        {
+            var LttProduct = LttProducts.FirstOrDefault(p => p.LttId == id);
+
+            if (LttProduct == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(LttProduct);
+        }
+
+        [HttpPost]
+        public ActionResult LttEdit(LttProduct lttProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(lttProduct);
+            }
+
+            var existingProduct = LttProducts.FirstOrDefault(p => p.LttId == lttProduct.LttId);
+
+            if (existingProduct == null)
+            {
+                return HttpNotFound();
+            }
+
+            existingProduct.LttName = lttProduct.LttName;
+            existingProduct.LttImage = lttProduct.LttImage;
+            existingProduct.LttQuantity = lttProduct.LttQuantity;
+            existingProduct.LttPrice = lttProduct.LttPrice;
+            existingProduct.LttIsActive = lttProduct.LttIsActive;
+
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult LttDelete(int id)
+        {
+            var LttProduct = LttProducts.FirstOrDefault(p => p.LttId == id);
+
+            if (LttProduct == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Confirmation page (optional)
+            // return View("DeleteConfirmation", LttProduct);
+
+            // Direct delete
+            LttProducts.Remove(LttProduct);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult LttDetail(int id)
+        {
+            var LttProduct = LttProducts.FirstOrDefault(p => p.LttId == id);
+
+            if (LttProduct == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(LttProduct);
+        }
+
+
+    }
+}
---
+++ LttBaiDanhGiaGiuaKy/LttBaiDanhGiaGiuaKy/Models/LttProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LttBaiDanhGiaGiuaKy.Models
+{
+    public class LttProduct
+    {
+            [Required(ErrorMessage = "Hãy nhập mã số")]
+            public int LttId { get; set; }
+            [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
+            [StringLength(100, MinimumLength = 5, ErrorMessage = "Tên sản phẩm phải dài từ 5 đến 100 ký tự")]
+            [RegularExpression("[A-Z ]+", ErrorMessage = "Tên sản phẩm chỉ chứa ký tự viết hoa và khoảng trắng")]
+            public string LttName { get; set; }
+            [Required(ErrorMessage = "Hãy nhập ảnh")]
+            public string LttImage { get; set; }
+            [Required(ErrorMessage = "Vui lòng nhập số lượng sản phẩm")]
+            [Range(1, 100, ErrorMessage = "Số lượng sản phẩm phải nằm trong khoảng từ 1 đến 100")]
+            public int LttQuantity { get; set; }
+            [Required(ErrorMessage = "Vui lòng nhập giá sản phẩm")]
+            [Range(0.01, double.MaxValue, ErrorMessage = "Giá sản phẩm phải lớn hơn 0")]
+            public decimal LttPrice { get; set; }
+            public bool LttIsActive { get; set; } = true;
+    }
+}
